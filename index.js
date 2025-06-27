d3afbce5f307863aeed6b66dac5380816cc7e1f5
percenterror <percent-error@proton.me>
2025-06-27 21:47:44 +0100

    do some major code refactoring

--- index.js
+++ index.js
@@ -1,31 +1,108 @@
+"use strict";
+
 const messageInput = document.getElementById("messageInput");
 const previewElement = document.getElementById("preview");
 
-function replaceUsingTags(inputString, oldVal, newVal){
-    let string = inputString;
-    let closing = false;
-    while (string.indexOf(oldVal) > -1){
-        // accounts for whether the character is escaped or not
-        const slashIndex = string.indexOf(oldVal) - 1;
-        if (string.indexOf(oldVal) > 0 && string[slashIndex] == "\\"){
-            string = string.slice(0, slashIndex) + string.slice(slashIndex + 1);
-            string = string.replace(oldVal, "&escapedFiller;");
+function multiStartsWith(string, array){
+    for (const item of array){
+        if (string.startsWith(item)){
+            return true
         }
-        else{
-            const newVal2 = "<" + "/".repeat(closing) + newVal.slice(1);
-            string = string.replace(oldVal, newVal2);
+    }
+    return false
+}
+
+function headerProperties(preview2){
+    let preview = preview2;
+    for (const i in preview){
+        let line = preview[i];
+        if (line.text.startsWith("# ")){
+            line.headerLevel = 1;
+            line.text = line.text.slice(2,);
+        }
+        else if (line.text.startsWith("## ")){
+            line.headerLevel = 2;
+            line.text = line.text.slice(3,);
+        }
+        else if (line.text.startsWith("### ")){
+            line.headerLevel = 3;
+            line.text = line.text.slice(4,);
+        }
+        else if (line.text.startsWith("-# ")){
+            line.headerLevel = -1;
+            line.text = line.text.slice(3,);
+        }
+        preview[i] = line;
+    }
+    return preview;
+}
+
+function listItemProperties(preview2){
+    let preview = preview2;
+    for (const i in preview){
+        const text = preview[i].text;
+        if (text.startsWith("- ") ||
+            text.startsWith("* ")){
+                preview[i] = {
+                    ...preview[i],
+                    "text": text.slice(2,),
+                    "isListItem": true
+                }
+        }
+    }
+    return preview;
+}
+
+function concatList(preview2){
+    const preview = preview2;
+    let stringPreview = "";
+    for (const line of preview){
+        let text = line.text;
+        if (line.headerLevel > 0){
+            text = "<h" + line.headerLevel + ">" 
+            + text
+            + "</h" + line.headerLevel + ">";
+        }
+        else if (line.headerLevel === -1){
+            text = "<small>" + text + "</small>";
+        }
+        if (line.isListItem){
+            text = "<li>" + text + "</li>";
+        }
+        if (multiStartsWith(text, ["<h", "<li>", "<small>"])){
+            stringPreview += text;
+        }
+        else {
+            // We don't have to worry about there being an extra <br> at the end because that won't be shown
+            stringPreview += text + "<br>";
+        }
+    }
+    return stringPreview;
+}
+
+function replaceMarkdown(preview2, markup2, tag2){
+    let preview = preview2;
+    const markup = markup2;
+    const tag = tag2;
+    const ctag = "</" + tag.slice(1,);
+    for (const line of preview){
+        let closing = false;
+        while (line.text.indexOf(markup) > -1){
+            if (closing){
+                line.text = line.text.replace(markup, ctag);
+            }
+            else{
+                line.text = line.text.replace(markup, tag);
+            }
             closing = !closing;
         }
     }
-    if (closing) {
-        string += "</" + newVal.slice(1);
-    }
-    string = string.replaceAll("&escapedFiller;", oldVal);
-    return string
+    return preview;
 }
 
 function updatePreview(){
-    let preview = messageInput.value;
+    let preview = messageInput.value ? messageInput.value
+                                     : "Sample Text";
 
     const unsafeCharacters = ["&", "<", ">", "="];
     const escapedCharacters = ["&amp;", "&lt;", "&gt;", "&equals;"]
@@ -34,63 +111,35 @@ function updatePreview(){
         preview = preview.replaceAll(unsafeCharacters[i], escapedCharacters[i]);
     }
 
-    // Accounts for newlines
-    preview = preview.replaceAll("\n", "<br>");
+    // Splits each line into seperate list elements, as 99% of markdown only works on a single line
+    preview = preview.split("\n");
 
-    // Replaces any bolds
-    preview = replaceUsingTags(preview, "**", "<b>");
+    // Adds properties to each line
+    for (const i in preview){
+        preview[i] = {
+            "text": preview[i],
+            "headerLevel": 0,
+            "isListItem": false,
+            "isPartOfCodeBlock": false
+        }
+    }
+    console.log(preview);
     
-    // Replaces any italics
-    preview = preview.replaceAll("<br>* ", "<br>&escapedListItem;");
-    preview = replaceUsingTags(preview, "*", "<i>");
-    preview = replaceUsingTags(preview, "__", "<u>");
-    preview = replaceUsingTags(preview, "_", "<i>");
-    preview = replaceUsingTags(preview, "~~", "<s>");
-    
-    // Replaces any mini code elements
-    preview = replaceUsingTags(preview, "`", "<code>");
+    // Adds line properties
+    preview = headerProperties(preview);
+    console.log(preview);
+    preview = listItemProperties(preview);
+    console.log(preview);
 
-    // Replaces any headings
-    preview = preview.split("<br>");
-    for (const i in preview){
-        if (preview[i][0] == "#"){
-            if (preview[i].indexOf(" ") <= 3){
-                const header = preview[i].indexOf(" ");
-                preview[i] = "<h" + header + ">" +
-                             preview[i].slice(header)
-                             + "</h" + header + ">";
-            }
-        }
-    }
-    for (const i in preview){
-        if (preview[i][0] == "-"){
-            if (preview[i][1] + preview[i][2] == "# "){
-                preview[i] = "<small>" + preview[i].slice(2) + "</small>";
-            }
-        }
-    }
-    for (const i in preview){
-        if (preview[i].indexOf("&escapedListItem;") == 0){
-            preview[i] = "<li>" + preview[i].slice(17,) + "</li>";
-        }
-    }
-    for (const i in preview){
-        if (preview[i].indexOf("- ") == 0){
-            preview[i] = "<li>" + preview[i].slice(2,) + "</li>";
-        }
-    }
-    preview = preview.join("<br>");
-    // Since headers and smalls already have line breaks, extra line breaks are removed
-    preview = preview.replaceAll("<h1><br>", "<h1>");
-    preview = preview.replaceAll("<h2><br>", "<h2>");
-    preview = preview.replaceAll("<h3><br>", "<h3>");
-    preview = preview.replaceAll("</h1><br>", "</h1>");
-    preview = preview.replaceAll("</h2><br>", "</h2>");
-    preview = preview.replaceAll("</h3><br>", "</h3>");
-    preview = preview.replaceAll("<small><br>", "<small>");
-    preview = preview.replaceAll("</small><br>", "</small>");
-    preview = preview.replaceAll("<li><br>", "<li>");
-    preview = preview.replaceAll("</li><br>", "</li>");
+    // Adds markdown
+    preview = replaceMarkdown(preview, "`", "<code>");
+    preview = replaceMarkdown(preview, "**", "<b>");
+    preview = replaceMarkdown(preview, "*", "<i>");
+    preview = replaceMarkdown(preview, "_", "<i>");
+    preview = replaceMarkdown(preview, "~~", "<s>");
+
+    // concat the list back into a string
+    preview = concatList(preview);
 
     previewElement.innerHTML = preview;
 }
