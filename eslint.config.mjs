27171b44c2553573f42ac86f5f4e973a7179d41e
percenterror <percent-error@proton.me>
2025-06-27 19:57:47 +0100

    add linter rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,16 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { rules: {
+    "complexity": ["error",
+                    {
+                      "max": 7,
+                      "variant": "modified"
+                    }],
+    "no-lonely-if": ["error"],
+    "no-global-assign": ["error"],
+    "no-shadow-restricted-names": ["error"],
+    "no-undefined": ["error"],
+    "no-unneeded-ternary": ["error"]
+  }}
 ]);
